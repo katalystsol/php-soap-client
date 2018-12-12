28d260843600473e1e8358bdee1c48e880f4cb52
Don Cranford <dcranford@katalystsolutions.com>
2018-12-12 13:39:57 -0600

    ComplexType - initial commit - WIP
    This is when the API requires a complex type / object to be sent

---
+++ src/ComplexType.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Katalystsol\PhpSoapClient;
+
+use Katalystsol\PhpSoapClient\Exceptions\SoapComplexTypeNotConfigured;
+
+
+abstract class ComplexType
+{
+    /** @var string Class name */
+    protected $complexTypeClassName;
+
+    /** @var array  */
+    protected $complexTypes = [];
+
+    /** @var array  */
+    protected $errors = [];
+//TODO what to do about collections() since I want it to be a generic package
+    /** @var \Illuminate\Support\Collection  */
+    protected $propertyValues;
+
+    /** @var array  */
+    protected $propertyKeys = [];
+
+    /** @var array  */
+    protected $values = [];
+
+    public function __construct(array $values)
+    {
+        $this->values = $values;
+        $this->propertyValues = collect([]);
+
+        $this->initialize();
+    }
+
+    /**
+     * Return the properties as an array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+//TODO how to handle this...?
+        return $this->propertyValues->toArray();
+    }
+
+    /**
+     * Initialize the properties for this class
+     */
+    protected function initialize()
+    {
+        $this->setPropertyKeys();
+        $this->setComplexTypes();
+        $this->setComplexTypeClassName();
+
+        foreach ($this->values as $key => $value) {
+            if (in_array($key, $this->propertyKeys)) {
+                try {
+                    $isComplexType = $this->isComplexType($key);
+                } catch (SoapComplexTypeNotConfigured $e) {
+                    $this->errors[] = $e->getMessage();
+                }
+
+                $propertyValue = $isComplexType ? $this->createComplexType($key)->toArray() : $value;
+//TODO collection?
+                $this->propertyValues->put($key, $propertyValue);
+            }
+        }
+    }
+
+    /**
+     * Check if the property is a complex type
+     *
+     * @param string $propertyKey
+     *
+     * @return bool
+     * @throws SoapComplexTypeNotConfigured
+     */
+    protected function isComplexType($propertyKey)
+    {
+        $isComplexType = strpos($propertyKey, 'obj') === 0;
+
+        if ($isComplexType && !$this->isComplexTypeConfigured($propertyKey)) {
+            $errorMessage = $propertyKey.' not configured for '.$this->complexTypeClassName;
+            throw new SoapComplexTypeNotConfigured($errorMessage);
+        }
+
+        return $isComplexType;
+    }
+
+    /**
+     * Check if the complex type is configured in the child class
+     *
+     * @param string $propertyKey
+     *
+     * @return bool
+     */
+    protected function isComplexTypeConfigured($propertyKey)
+    {
+        return array_key_exists($propertyKey, $this->complexTypes);
+    }
+
+    /**
+     * Instantiate and return a complex type object
+     *
+     * @param string $propertyKey
+     *
+     * @return ComplexType
+     */
+    protected function createComplexType($propertyKey)
+    {
+        $complexTypeClassName = $this->complexTypes[$propertyKey];
+
+        return new $complexTypeClassName($this->values);
+    }
+
+    /**
+     * Set the properties array with the properties / keys needed for the class
+     */
+    abstract protected function setPropertyKeys();
+
+    /**
+     * Set the complex types array
+     */
+    abstract protected function setComplexTypes();
+
+    /**
+     * Sets the class name for the complex type class
+     */
+    abstract protected function setComplexTypeClassName();
+}
